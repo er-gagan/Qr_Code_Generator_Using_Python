169a3fdebcfbed462a9e783dbe29110c3423d954
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-05-28 18:25:55 +0530

    Frontend complete
    Complete frontend design

---
+++ QRCode/Qr Code Generator/main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+root = Tk()
+root.geometry("390x350")
+root.title("Qr Code Generator")
+
+Ph = PhotoImage(file="./ico.png")
+root.iconphoto(False,Ph)
+
+Label(root,text="QR Code Generator",font=("Calibri",25)).place(x=60,y=10)
+
+Label(root,text="Enter Some Data",font=("Calibri",15)).place(x=120,y=80)
+e1 = Entry(root,font=("Calibri",20))
+e1.focus()
+e1.place(x=50,y=120)
+
+Button(root,text="Click Here!",font=("Calibri",20)).place(x=120,y=190)
+
+Button(root,text="Clear",font=("Calibri",15)).place(x=160,y=280)
+
+root.mainloop()
